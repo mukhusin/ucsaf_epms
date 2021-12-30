d1acb8924a1e5f2f87c23fd3e3a9df2f9e0714d9
mukhusin <mukhusinsiraji@gmail.com>
2021-12-30 23:52:56 +0300

    login validation

--- ucsafepms/authentication/urls.py
+++ ucsafepms/authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 urlpatterns = [
     path('login', views.login, name="login"),
-    path('reset-password', views.reset_password, name="reset-password")
+    path('reset-password', views.reset_password, name="reset-password"),
+    path('validate-username', csrf_exempt(views.UsernameValidationView.as_view()), name="validate-username")
 ]
--- ucsafepms/authentication/views.py
+++ ucsafepms/authentication/views.py
@@ -1,5 +1,9 @@
+import json
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views import View
+
+# from ucsafepms.authentication.models import Users
 # Create your views here.
 
 def login(request):
@@ -7,3 +11,13 @@ def login(request):
 
 def reset_password(request):
     return render(request,'authentication/reset-password.html')
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'},status=409)
+        # if Users.objects.filter(username=username).exists():
+        #     return JsonResponse({'username_error': 'Sorry username in use, choose another one'},status=400)
+        return JsonResponse({'username_valid': True})
