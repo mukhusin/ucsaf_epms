4ba7a1e374535301a22a4e51ad2287a914fbd573
mukhusin <mukhusinsiraji@gmail.com>
2021-12-30 15:01:09 +0300

    first commit

--- ucsafepms/authentication/views.py
+++ ucsafepms/authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+def login(request):
+    return render(request,'authentication/login.html')
+
+def reset_password(request):
+    return render(request,'authentication/reset-password.html')
